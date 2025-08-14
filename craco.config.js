90351d63c21917f3cf9a81035719c23a86933f72
ram meenavalli <rammeenavalli@rams-MacBook-Air.local>
2025-08-14 13:22:33 -0700

    Resolve merge conflict in craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -7,4 +7,14 @@ module.exports = {
       ],
     },
   },
+<<<<<<< Updated upstream
+=======
+  webpack: {
+    configure: (webpackConfig) => {
+      // Build to standard build directory for GitHub Pages
+      webpackConfig.output.path = path.resolve(__dirname, 'build');
+      return webpackConfig;
+    },
+  },
+>>>>>>> Stashed changes
 };
